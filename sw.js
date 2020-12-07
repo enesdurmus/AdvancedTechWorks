d077a2bfa12de9a663f39e65434344938effb038
Enes Durmus <66317017+enesdurmus@users.noreply.github.com>
2020-12-07 17:00:34 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,33 @@
-const CACHE = 'JS'
-const FILES = ['/index.html', ...];
+const CACHE ='AP'
 function installCB(e) {
-    e.waitUntil(
-        caches.open(CACHE)
-            .then(cache => cache.addAll(FILES))
-            .catch(console.log)
-    )
+  console.log(CACHE, e);
 }
-self.addEventListener('install', installCB)
-
-function cacheCB(e) { //cache first
-    let req = e.request
-    e.respondWith(
-        caches.match(req)
-            .then(r1 => r1 || fetch(req))
-            .catch(console.log)
-    )
-}
-self.addEventListener('fetch', cacheCB)
+addEventListener('install', installCB)
 
 function save(req, resp) {
-    return caches.open(CACHE)
-        .then(cache => {
-            cache.put(req, resp.clone());
-            return resp;
-        })
-        .catch(console.log)
+  if (!req.url.includes("github")) 
+     return resp;
+  return caches.open(CACHE)
+  .then(cache => { // save request
+    cache.put(req, resp.clone());
+    return resp;
+  }) 
+  .catch(console.err)
+}
+function report(req) {
+  console.log(CACHE+' matches '+req.url)
+  return req
 }
 function fetchCB(e) { //fetch first
-    let req = e.request
-    e.respondWith(
-        fetch(req).then(r2 => save(req, r2))
-            .catch(() => { return caches.match(req).then(r1 => r1) })
-    )
+  let req = e.request
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => caches.match(req).then(report))
+  )
 }
-self.addEventListener('fetch', fetchCB)
+addEventListener('fetch', fetchCB)
+
+function activateCB(e) {
+  console.log(CACHE, e);
+}
+addEventListener('activate', activateCB);
